1b8449002196a7bd261c08410ebfe78eee2ff65a
Kaspar L. Palgi <git@e-stonia.co.uk>
2026-04-02 10:35:02 +0300

    feat(playwright): configure for dev server and to use local chrome

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-	webServer: { command: 'npm run build && npm run preview', port: 4173 },
-	testMatch: '**/*.e2e.{ts,js}'
+	webServer: {
+		command: 'npm run dev', // For production: 'npm run build && npm run preview',
+		port: 5173, // 4173 for production
+		reuseExistingServer: true
+	},
+	testMatch: '**/*.e2e.{ts,js}',
+
+	use: {
+		baseURL: 'http://localhost:5173',
+		channel: 'chrome',
+		headless: false // set `true` if you don't want to see the browser to be opened
+	}
 });
